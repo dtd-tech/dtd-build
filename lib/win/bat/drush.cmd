61f612bb4fc34aced47e887d94dcb8ba332ce66d
SqyD <paul@krischer.nl>
2015-10-04 18:56:52 +0200

    Batch fixes, phpmadmin vhost, added static dtd.pac file

--- lib/win/bat/drush.cmd
+++ lib/win/bat/drush.cmd
@@ -5,3 +5,9 @@ set "dtddir=%cd%"
 
 doskey drush=%dtddir%\core\php55\php.exe %dtddir%\core\drush\drush.php --alias-path=%dtddir\core\drush\aliases $*
 cd tracks
+ECHO DTD-Drush command line
+ECHO Aliases:
+drush site-alias
+ECHO Examples:
+ECHO drush help
+ECHO drush @main status
